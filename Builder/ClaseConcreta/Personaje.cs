09e7a3351320be7a5cadaaeff5115100f63fe024
Alesandro Maldonado <alegm2010@gmail.com>
2023-03-11 08:27:37 -0300

    11/3/23 8:27 Agregando patron Builder

---
+++ Builder/ClaseConcreta/Personaje.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    // La clase Personaje representa el objeto que queremos construir
+    public class Personaje
+    {
+        public string Cabello { get; set; }
+        public string Ojos { get; set; }
+        public string ColorPiel { get; set; }
+        public string Raza { get; set; }
+
+        public Personaje(string cabello, string ojos, string colorPiel, string raza)
+        {
+            Cabello = cabello;
+            Ojos = ojos;
+            ColorPiel = colorPiel;
+            Raza = raza;
+        }
+    }
+}
---
+++ Builder/interfaces/IPersonajeBuilder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    // La interfaz IPersonajeBuilder define los métodos necesarios para construir un objeto Personaje
+    public interface IPersonajeBuilder
+    {
+        void SetCabello(string cabello);
+        void SetOjos(string ojos);
+        void SetColorPiel(string colorPiel);
+        void SetRaza(string raza);
+        Personaje Build();
+    }
+}
