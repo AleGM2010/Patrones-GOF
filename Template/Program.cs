d3e36fb72547c438518cae1a3ef7652678d2f5a2
Alesandro Maldonado <alegm2010@gmail.com>
2023-05-12 16:37:51 -0300

    Agregando Patron Template 12/5 16:37

---
+++ Template/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Template
+{
+    namespace RPGGame
+    {
+        public abstract class CharacterTemplate
+        {
+            public void PlayTurn()
+            {
+                Move();
+                if (this is Bard)
+                {
+                    AditionalAction();
+                }
+                else
+                {
+                    Attack();
+                }
+
+                EndTurn();
+            }
+            protected virtual void Move()
+            {
+                Console.WriteLine("Moving...");
+            }
+
+            protected virtual void Attack() { }
+
+            protected virtual void EndTurn()
+            {
+                Console.WriteLine("Ending turn...");
+            }
+
+            protected virtual void AditionalAction(){ }
+        }
+
+        public class Warrior : CharacterTemplate
+        {
+            protected override void Attack()
+            {
+                Console.WriteLine("Warrior is using a melee attack!");
+            }
+        }
+
+        public class Mage : CharacterTemplate
+        {
+            protected override void Attack()
+            {
+                Console.WriteLine("Mage is casting a spell!");
+            }
+        }
+
+        public class Bard : CharacterTemplate
+        {
+            // propiedades y métodos existentes aquí
+            
+            protected override void AditionalAction()
+            {
+                // El bardo no ataca, así que no hay implementación aquí.
+                // En su lugar, se llama al método Sing.
+                Sing();
+            }
+
+            private void Sing()
+            {
+                Console.WriteLine("Bard is singing!.");
+            }
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                var warrior = new Warrior();
+                var mage = new Mage();
+                var bard = new Bard();
+
+                Console.WriteLine("Warrior's turn:");
+                warrior.PlayTurn();
+
+                Console.WriteLine("\nMage's turn:");
+                mage.PlayTurn();
+
+                Console.WriteLine("\nBard's turn:");
+                bard.PlayTurn();
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
